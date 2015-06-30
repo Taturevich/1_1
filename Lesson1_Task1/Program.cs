838224feb94d003cd2904f8b929a950327ceccaa
X_Finder <pollux094@gmail.com>
2015-06-30 13:30:49 +0200

    Добавление

---
+++ Lesson1_Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объявление объектов класса
+            TravelExpenses[] mission = new TravelExpenses[5];
+            mission[0] = new TravelExpenses("Ivar Maksutov", 40000, 18);
+            mission[1] = new TravelExpenses("Maxim Valov", 38000, 16);
+            mission[3] = new TravelExpenses("Zahar Baturin", 51000, 25);
+            mission[4] = new TravelExpenses();
+
+            //Вывод информации об объектах методом Show()
+            for (int i = 0; i < mission.Length; i++)
+            {
+                if (i != 2)
+                {
+                    Console.WriteLine("Информация о командирвке №" + (i + 1));
+                    mission[i].Show();
+                    Console.WriteLine();
+                }
+                
+            }
+
+            //Изменение транспортных расходов в последнем элементе массива
+            mission[4].Fare=100000;
+
+            //Вывод суммарного количества дней первых двух командировок
+            Console.WriteLine("Общая продолжительность первых двух командировок: "+(mission[0].Days+mission[1].Days)+" дня(ей)\n");
+
+            //Вывод массива на консоль с использованием метода ToString()
+            Console.WriteLine("Информация в csv формате:\n");
+            for (int i = 0; i < mission.Length; i++)
+            {
+                if (i != 2)
+                {
+                    Console.WriteLine(mission[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson1_Task1/TravelExpenses.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_Task1
+{
+    class TravelExpenses
+    {
+        public const int per_diem = 85000; // суточные в бел.руб
+        string initials; // фамилия и имя работника
+        int fare; // транспортные расходы в бел.руб
+        int days; // количество дней
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public TravelExpenses()
+        {
+            initials="Marat Kazey"; 
+            fare=120000; 
+            days=31; 
+        }
+
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="initials"></param>
+        /// <param name="fare"></param>
+        /// <param name="days"></param>
+        public TravelExpenses(string initials, int fare, int days)
+        {
+            this.initials =initials;
+            this.fare = fare;
+            this.days = days;
+        }
+
+
+        //Геттеры и сеттеры
+
+        public string Initials
+        {
+            get { return initials; }
+            set { this.initials = value; }
+        }
+
+        public int Fare
+        {
+            get { return fare; }
+            set { this.fare = value; }
+        }
+
+        public int Days
+        {
+            get { return days; }
+            set { this.days = value; }
+        }
+
+        /// <summary>
+        /// Расчёт общей величины расходов
+        /// </summary>
+        /// <returns></returns>
+        public int GetTotal()
+        {
+            int total;
+            total = fare + days * per_diem;
+            return total;
+        }
+
+        /// <summary>
+        /// Вывод всех полей в консоли по одному
+        /// </summary>
+        public void Show()
+        {
+            Console.WriteLine("per_diem="+per_diem);
+            Console.WriteLine("initials="+initials);
+            Console.WriteLine("fare="+fare);
+            Console.WriteLine("days="+days);
+            Console.WriteLine("total="+GetTotal());
+        }
+
+        /// <summary>
+        /// Вывод информации в csv формате
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return per_diem + ";" + initials + ";" + fare + ";" + days + ";" + GetTotal() + ";";
+        }
+
+
+    }
+}
